70f7386cf50df0e09380d56e9280d48e3fc2f3bf
nengberg <per.niklas.engberg@gmail.com>
2015-10-10 15:58:30 +0200

    Organized files.

--- index.js
+++ index.js
@@ -5,12 +5,12 @@ app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
-// views is directory for all template files
-app.set('views', __dirname + '/pages');
+app.set('views', __dirname + '/views/pages');
 app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
 
 app.get('/', function(request, response) {
-  response.render('index');
+  response.render('index.html');
 });
 
 app.listen(app.get('port'), function() {
