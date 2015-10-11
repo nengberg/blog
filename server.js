f00acfdadc2618ccaa3e6ae580c8f82d7b03cdcf
nengberg <per.niklas.engberg@gmail.com>
2015-10-11 14:19:17 +0200

    Organized files, structured app.

---
+++ server.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var app = express();
+
+//Configure views
+app.set('views', __dirname + '/app/views');
+app.set('view engine', 'ejs');
+app.engine('html', require('ejs').renderFile);
+
+//Configure static resources
+app.use("/css", express.static(__dirname + '/app/css'));
+app.use("/img", express.static(__dirname + '/app/img'));
+app.use("/font", express.static(__dirname + '/app/font'));
+
+var routes = require('./app/routes/routes.js');
+routes(app);
+
+app.listen(3000, function() {
+	console.log('Server running on port 3000')
+});
